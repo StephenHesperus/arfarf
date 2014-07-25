fcd9e9c8080dac2f8eed87ceabc5383113464e1f
Ste H. W <Ste at gmail.com>
2014-07-25 21:56:36 +0800

    Implement Dog._parse_gitignore()
    See TODO file for the prerequisite to use this method.

--- test_wdog.py
+++ test_wdog.py
@@ -1,4 +1,8 @@
+import os
 import unittest
+from unittest.mock import mock_open, patch
+
+from wdog import Dog
 
 
 class DogTestCase(unittest.TestCase):
@@ -12,3 +16,12 @@ class DogTestCase(unittest.TestCase):
             dog(patterns=['*.py'], command='echo hello', use_gitignore=True)
         except:
             self.fail(__doc__)
+
+    def test__parse_gitignore(self):
+        dog = Dog()
+        m = mock_open(read_data='*.py[cod]\n# comment line\n__pycache__/\n')
+        with patch('builtins.open', m, create=True) as mock:
+            result = dog._parse_gitignore()
+        patterns = ['*.py[cod]', '__pycache__/']
+        mock.assert_called_once_with(os.path.join(os.getcwd(), '.gitignore'))
+        self.assertEqual(patterns, result)
--- wdog.py
+++ wdog.py
@@ -1,3 +1,6 @@
+import os
+
+
 class Dog(object):
     def __init__(self, command='', patterns=['*'], ignore_patterns=[],
                  ignore_directories=False, path='.', recursive=True,
@@ -9,3 +12,12 @@ class Dog(object):
         self._path = path
         self._recursive = recursive
         self._use_gitignore = use_gitignore
+
+    def _parse_gitignore(self):
+        with open(os.path.join(os.getcwd(), '.gitignore')) as f:
+            lines = f.readlines()
+        gitignore = []
+        for line in lines:
+            if not line.startswith('#'):
+                gitignore.append(line.strip())
+        return gitignore
